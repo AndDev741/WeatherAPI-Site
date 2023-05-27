0a8d080b16850d0b5e4b96add272c0fabba21835
AndDev <133960519+AndDev741@users.noreply.github.com>
2023-05-27 20:00:24 -0300

    Update script.js
    Added the function isNigth( ) to change the icon when its... Nigth.

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const backgroundImages = [
     document.body.style.backgroundImage = `url(${attBackground})`;
 
 
-
+/* Links paras as imagens
 const linksIcons = [
     rain = 'https://i.pinimg.com/564x/84/b2/b3/84b2b3e2e7f855abdeec95b285b553ca.jpg',
     rainWithSun = 'https://i.pinimg.com/564x/b6/12/41/b61241b089a9a5ad6c721e4651f84402.jpg',
@@ -33,6 +33,34 @@ const linksIcons = [
     snow = 'https://i.pinimg.com/564x/58/fe/d6/58fed611e8129a3f267b78a31c2aa53a.jpg',
     sun = 'https://i.pinimg.com/564x/5d/92/37/5d9237e420b69e478fa390cf545d1c22.jpg',
 ]
+*/
+
+//Constantes para as imagens
+
+const hazeSM = [{
+    sun: 'https://i.pinimg.com/564x/fe/8e/d9/fe8ed9a053a4d1a45e7d3a889a40e415.jpg',
+    moon: 'https://i.pinimg.com/736x/5f/e3/68/5fe36863d606a42631122162478884f8.jpg'
+  }]
+
+const cloudsSM = {
+    sun: 'https://i.pinimg.com/564x/49/8f/f8/498ff8630d974835b6b693aeaaa383d1.jpg',
+    moon: 'https://i.pinimg.com/564x/4b/18/b3/4b18b38c0fb08ee69587d0cd4c559a5a.jpg'
+}
+
+const clearSM = {
+    sun: 'https://i.pinimg.com/564x/5d/92/37/5d9237e420b69e478fa390cf545d1c22.jpg',
+    moon: 'https://i.pinimg.com/564x/0c/3f/e8/0c3fe8f3feac5abc1727d7d83cbcb40f.jpg'
+}
+
+const rainSM = {
+    sun: 'https://i.pinimg.com/564x/84/b2/b3/84b2b3e2e7f855abdeec95b285b553ca.jpg',
+    moon: 'https://i.pinimg.com/564x/3b/ef/d6/3befd66c3ee6fc15e674542e7cc772d5.jpg'
+}
+
+const snowSM = {
+    sun: 'https://i.pinimg.com/564x/58/fe/d6/58fed611e8129a3f267b78a31c2aa53a.jpg',
+    moon: 'https://i.pinimg.com/564x/84/ae/5f/84ae5feedd58c161b9995c30e83a71f2.jpg'
+}
 
 
 //Código com api
@@ -43,6 +71,16 @@ input.addEventListener('keyup', function (event) {
         fetchWeatherData(city)
     }
   })
+
+//Função para ver se está de noite ou de dia
+  function isNigth() {
+    const currentHour = new Date().getHours();
+    return currentHour >= 18 || currentHour < 6;
+  }
+
+  
+
+
   apiKey = 'a3af4e77136c83de9e0d61dda95f8c3b'
 
   function fetchWeatherData(city) {
@@ -66,22 +104,46 @@ input.addEventListener('keyup', function (event) {
 
   switch(json.weather[0].main){
     case 'Haze':
-        image.src = 'https://i.pinimg.com/564x/fe/8e/d9/fe8ed9a053a4d1a45e7d3a889a40e415.jpg';
+        if(isNigth()){
+            image.src = hazeSM.moon
+        } else {
+            image.src = hazeSM.sun
+        }
         break;
     case 'Clear':
-        image.src = 'https://i.pinimg.com/564x/5d/92/37/5d9237e420b69e478fa390cf545d1c22.jpg';
+        if(isNigth()){
+            image.src = clearSM.moon
+        } else {
+            image.src = clearSM.sun
+        }
         break;
     case 'Rain':
-        image.src = 'https://i.pinimg.com/564x/84/b2/b3/84b2b3e2e7f855abdeec95b285b553ca.jpg';
+        if(isNigth()){
+            image.src = rainSM.moon
+        } else {
+            image.src = rainSM.sun
+        }
         break;
     case 'Drizzle':
-        image.src = 'https://i.pinimg.com/564x/b6/12/41/b61241b089a9a5ad6c721e4651f84402.jpg';
+        if(isNigth()){
+            image.src = rainSM.moon
+        } else {
+            image.src = rainSM.sun
+        };
         break;
     case 'Clouds':
-        image.src = 'https://i.pinimg.com/564x/49/8f/f8/498ff8630d974835b6b693aeaaa383d1.jpg';
+        if(isNigth()){
+            image.src = cloudsSM.moon;
+        } else {
+            image.src = cloudsSM.sun;
+        };
         break;
     case 'Snow':
-        image.src = 'https://i.pinimg.com/564x/58/fe/d6/58fed611e8129a3f267b78a31c2aa53a.jpg';
+        if(isNigth()){
+            image.src = snowSM.moon;
+        } else {
+            image.src = snowSM.sun;
+        };
         break;
     case 'Default':
     image.src = '';
